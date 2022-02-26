2cc57849f5578cdad9f0f0466fa524908a7f3eae
Waterlens <82268070+waterlens@users.noreply.github.com>
2022-02-26 17:37:33 +0800

    update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
+use either::Either;
+use either::Left;
+use either::Right;
 use petgraph::algo::is_cyclic_directed;
 use petgraph::stable_graph::NodeIndex;
 use petgraph::stable_graph::StableDiGraph;
+use petgraph::visit::Bfs;
 use regex::Regex;
 use std::collections::HashMap;
-use std::iter::Peekable;
+use std::fmt::Debug;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub enum Associativity {
     Left,
     Right,
     None,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub enum Fixity {
     Prefix,
     Infix(Associativity),
@@ -26,10 +30,10 @@ pub struct Operator {
     pub name: String,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct NamePart(Vec<Option<String>>);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct OperatorPart {
     pub fixity: Fixity,
     pub name: String,
@@ -46,7 +50,7 @@ impl Operator {
 
 impl From<&str> for NamePart {
     fn from(s: &str) -> Self {
-        let mut result = Vec::new();
+        let mut result = vec![];
         let mut last = 0;
         for (index, matched) in s.match_indices('_') {
             if last != index {
@@ -58,6 +62,15 @@ impl From<&str> for NamePart {
         if last < s.len() {
             result.push(Some(s[last..].to_string()));
         }
+
+        if let Some(None) = result.last() {
+            result.pop();
+        }
+
+        if let Some(None) = result.first() {
+            result.remove(0);
+        }
+
         NamePart(result)
     }
 }
@@ -65,7 +78,7 @@ impl From<&str> for NamePart {
 impl OperatorPart {
     pub fn new(op: &Operator) -> Self {
         let name = op.name.to_string();
-        let fixity = op.fixity.clone();
+        let fixity = op.fixity;
         let part = op.name.as_str().into();
         Self { fixity, name, part }
     }
@@ -79,13 +92,49 @@ pub enum PrecedenceRelation {
     Undefined,
 }
 
+pub trait PrecedenceGraph {
+    type P: Copy;
+    fn ops(&self, prec: Self::P, fix: Fixity) -> Vec<&OperatorPart>;
+    fn succ(&self, prec: Self::P) -> Vec<Self::P>;
+    fn all(&self) -> Vec<Self::P>;
+}
+
+impl PrecedenceGraph for StableDiGraph<Vec<OperatorPart>, ()> {
+    type P = NodeIndex;
+
+    fn ops(&self, prec: Self::P, fix: Fixity) -> Vec<&OperatorPart> {
+        self[prec].iter().filter(|o| o.fixity == fix).collect()
+    }
+
+    fn succ(&self, prec: Self::P) -> Vec<Self::P> {
+        let mut succ = vec![];
+        let mut bfs = Bfs::new(self, prec);
+        while let Some(n) = bfs.next(self) {
+            if n != prec {
+                succ.push(n);
+            }
+        }
+        succ
+    }
+
+    fn all(&self) -> Vec<Self::P> {
+        self.node_indices().collect()
+    }
+}
+
 #[derive(Debug)]
-pub struct PrecedenceGraph {
+pub struct MappedPrecedenceGraph {
     graph: StableDiGraph<Vec<OperatorPart>, ()>,
     map: HashMap<String, NodeIndex>,
 }
 
-impl PrecedenceGraph {
+impl Default for MappedPrecedenceGraph {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl MappedPrecedenceGraph {
     pub fn new() -> Self {
         Self {
             graph: StableDiGraph::new(),
@@ -158,28 +207,430 @@ impl PrecedenceGraph {
 
 pub type ParserResult<I, O, E> = Result<(I, O), E>;
 
+pub trait ParserInput: PartialEq + Clone + Debug {
+    fn take(&self, n: usize) -> Option<Self>;
+    fn take_split(&self, n: usize) -> Option<(Self, Self)>;
+    fn take_split_if(&self, f: impl FnOnce(Self) -> bool, n: usize) -> Option<(Self, Self)>;
+    fn size(&self) -> usize;
+}
+
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     UnexpectedToken(usize),
     UnexpectedEndOfInput,
+    UnparsedInput,
 }
 
-pub trait Input: Sized {
-    fn take(&self, n: usize) -> Self;
-    fn take_split(&self, n: usize) -> (Self, Self);
+pub trait Compare<T> {
+    fn compare(&self, other: T) -> bool;
 }
 
-pub trait Parser {
-    type I: Input;
-    type O;
-    fn parse(&self, i: Self::I) -> ParserResult<Self::I, Self::O, ParseError>;
+impl<'a> ParserInput for &'a str {
+    fn take(&self, n: usize) -> Option<Self> {
+        if self.len() >= n {
+            Some(&self[..n])
+        } else {
+            None
+        }
+    }
+    fn take_split(&self, n: usize) -> Option<(Self, Self)> {
+        if self.len() >= n {
+            Some((&self[..n], &self[n..]))
+        } else {
+            None
+        }
+    }
+    fn take_split_if(&self, f: impl FnOnce(Self) -> bool, n: usize) -> Option<(Self, Self)> {
+        if self.len() >= n && f(&self[..n]) {
+            Some((&self[..n], &self[n..]))
+        } else {
+            None
+        }
+    }
+    fn size(&self) -> usize {
+        self.len()
+    }
 }
 
-impl<P: Parser> Parser for &P {
-    type I = P::I;
-    type O = P::O;
-    fn parse(&self, i: Self::I) -> ParserResult<Self::I, Self::O, ParseError> {
+pub trait Parser<I, O>
+where
+    I: ParserInput,
+{
+    fn parse(&self, i: I) -> ParserResult<I, O, ParseError>;
+}
+
+impl<I, O, P> Parser<I, O> for &P
+where
+    I: ParserInput,
+    P: Parser<I, O>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, O, ParseError> {
         (*self).parse(i)
+    }
+}
+
+impl<I, O> Parser<I, O> for &dyn Parser<I, O>
+where
+    I: ParserInput,
+{
+    fn parse(&self, i: I) -> ParserResult<I, O, ParseError> {
+        (*self).parse(i)
+    }
+}
+
+pub struct SkipSpace();
+impl<'a, I> Parser<I, ()> for SkipSpace
+where
+    I: ParserInput + Compare<&'a str>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, (), ParseError> {
+        let mut i = i;
+        while let Some((_, rest)) = i.take_split_if(|s| s.compare(" "), 1) {
+            i = rest;
+        }
+        Ok((i, ()))
+    }
+}
+
+pub struct Tag<T>(T);
+impl<I, T> Parser<I, ()> for Tag<T>
+where
+    I: ParserInput + Compare<T>,
+    T: ParserInput,
+{
+    fn parse(&self, i: I) -> ParserResult<I, (), ParseError> {
+        let t = self.0.clone();
+        if let Some((tok, rest)) = i.take_split(self.0.size()) {
+            if tok.compare(t) {
+                Ok((rest, ()))
+            } else {
+                Err(ParseError::UnexpectedToken(rest.size()))
+            }
+        } else {
+            Err(ParseError::UnexpectedEndOfInput)
+        }
+    }
+}
+
+impl<I, O1, O2, P1, P2> Parser<I, (O1, O2)> for (P1, P2)
+where
+    I: ParserInput,
+    P1: Parser<I, O1>,
+    P2: Parser<I, O2>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, (O1, O2), ParseError> {
+        let (p1, p2) = self;
+        let (rest, o1) = p1.parse(i)?;
+        let (rest, o2) = p2.parse(rest)?;
+        Ok((rest, (o1, o2)))
+    }
+}
+impl<I, O1, O2, O3, P1, P2, P3> Parser<I, (O1, O2, O3)> for (P1, P2, P3)
+where
+    I: ParserInput,
+    P1: Parser<I, O1>,
+    P2: Parser<I, O2>,
+    P3: Parser<I, O3>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, (O1, O2, O3), ParseError> {
+        let (p1, p2, p3) = self;
+        let (rest, o1) = p1.parse(i)?;
+        let (rest, o2) = p2.parse(rest)?;
+        let (rest, o3) = p3.parse(rest)?;
+        Ok((rest, (o1, o2, o3)))
+    }
+}
+
+struct Or<P1, P2>(P1, P2);
+impl<I, O1, O2, P1, P2> Parser<I, Either<O1, O2>> for Or<P1, P2>
+where
+    I: ParserInput,
+    P1: Parser<I, O1>,
+    P2: Parser<I, O2>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Either<O1, O2>, ParseError> {
+        let (p1, p2) = (&self.0, &self.1);
+        match (p1.parse(i.clone()), p2.parse(i.clone())) {
+            (Ok((rest1, o1)), Ok((rest2, o2))) => {
+                if rest1.size() < rest2.size() {
+                    Ok((rest1, Left(o1)))
+                } else {
+                    Ok((rest2, Right(o2)))
+                }
+            }
+            (Ok((rest, o1)), _) => Ok((rest, Left(o1))),
+            (_, Ok((rest, o2))) => Ok((rest, Right(o2))),
+            _ => Err(ParseError::UnexpectedToken(i.size())),
+        }
+    }
+}
+
+struct Alt<P>(Vec<P>);
+impl<I, O, P> Parser<I, O> for Alt<P>
+where
+    I: ParserInput,
+    P: Parser<I, O>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, O, ParseError> {
+        let mut oks = vec![];
+        for p in &self.0 {
+            if let Ok(ok) = p.parse(i.clone()) {
+                oks.push(ok);
+            }
+        }
+        let r = oks.into_iter().min_by_key(|(rest, _)| rest.size());
+        r.ok_or_else(|| ParseError::UnexpectedToken(i.size()))
+    }
+}
+
+struct Seq<P>(Vec<P>);
+impl<I, O, P> Parser<I, Vec<O>> for Seq<P>
+where
+    I: ParserInput,
+    P: Parser<I, O>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Vec<O>, ParseError> {
+        let mut s = i;
+        let mut r = vec![];
+        for p in &self.0 {
+            let (i, o) = p.parse(s)?;
+            r.push(o);
+            s = i;
+        }
+        Ok((s, r))
+    }
+}
+
+struct Many1<P>(P);
+impl<I, O, P> Parser<I, Vec<O>> for Many1<P>
+where
+    I: ParserInput,
+    P: Parser<I, O>,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Vec<O>, ParseError> {
+        let (mut s, o) = self.0.parse(i)?;
+        let mut v = vec![o];
+        while let Ok((rest, o)) = self.0.parse(s.clone()) {
+            s = rest;
+            v.push(o);
+        }
+        Ok((s, v))
+    }
+}
+
+#[derive(Debug)]
+pub struct Tree {
+    pub op: OperatorPart,
+    pub input: Vec<Tree>,
+}
+
+struct Expr<'a, G: PrecedenceGraph>(&'a G);
+impl<'a, I, G> Parser<I, Tree> for Expr<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        Precs(self.0, self.0.all()).parse(i)
+    }
+}
+
+struct Precs<'a, G: PrecedenceGraph>(&'a G, Vec<G::P>);
+impl<'a, I, G> Parser<I, Tree> for Precs<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        let prec = self.1.iter().map(|&p| Prec(self.0, p)).collect();
+        Alt(prec).parse(i)
+    }
+}
+
+struct Op<'a, G: PrecedenceGraph>(&'a G, &'a OperatorPart);
+impl<'a, I, G> Parser<I, Tree> for Op<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        let op = self.1;
+        let parts = &op.part.0;
+        let mut vec = vec![];
+        let mut next = i;
+        for part in parts {
+            if let Some(s) = part {
+                let (rest, _) = SkipSpace().parse(next)?;
+                let (rest, _) = Tag(s.as_str()).parse(rest)?;
+                next = rest;
+            } else {
+                // hole
+                let (rest, o) = Expr(self.0).parse(next)?;
+                vec.push(o);
+                next = rest;
+            }
+        }
+
+        Ok((
+            next,
+            Tree {
+                op: op.clone(),
+                input: vec,
+            },
+        ))
+    }
+}
+
+struct Inner<'a, G: PrecedenceGraph>(&'a G, G::P, Fixity);
+impl<'a, I, G> Parser<I, Tree> for Inner<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        Alt(self
+            .0
+            .ops(self.1, self.2)
+            .into_iter()
+            .map(|op| Op(self.0, op))
+            .collect())
+        .parse(i)
+    }
+}
+
+struct Prec<'a, G: PrecedenceGraph>(&'a G, G::P);
+impl<'a, I, G> Parser<I, Tree> for Prec<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        Alt::<&dyn Parser<I, Tree>>(vec![
+            &Closed(self.0, self.1),
+            &InfixNonAssoc(self.0, self.1),
+            &PreRight(self.0, self.1),
+            &PostLeft(self.0, self.1),
+        ])
+        .parse(i)
+    }
+}
+
+struct InfixNonAssoc<'a, G: PrecedenceGraph>(&'a G, G::P);
+impl<'a, I, G> Parser<I, Tree> for InfixNonAssoc<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        let succ = Precs(self.0, self.0.succ(self.1));
+        let (i, left) = succ.parse(i)?;
+        let (i, mut expr) = Inner(self.0, self.1, Fixity::Infix(Associativity::None)).parse(i)?;
+        let (i, right) = succ.parse(i)?;
+        expr.input.insert(0, left);
+        expr.input.push(right);
+        Ok((i, expr))
+    }
+}
+
+struct Closed<'a, G: PrecedenceGraph>(&'a G, G::P);
+impl<'a, I, G> Parser<I, Tree> for Closed<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        Inner(self.0, self.1, Fixity::Closed).parse(i)
+    }
+}
+
+struct PreRight<'a, G: PrecedenceGraph>(&'a G, G::P);
+impl<'a, I, G> Parser<I, Tree> for PreRight<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        let succ = Precs(self.0, self.0.succ(self.1));
+        let pre = Or(
+            Inner(self.0, self.1, Fixity::Prefix),
+            (
+                &succ,
+                Inner(self.0, self.1, Fixity::Infix(Associativity::Right)),
+            ),
+        );
+        let (rest, inners) = Many1(pre).parse(i)?;
+        let (rest, succ) = succ.parse(rest)?;
+        let mut expr = inners
+            .into_iter()
+            .map(|e| {
+                e.map_right(|(first, mut rest)| {
+                    rest.input.insert(0, first);
+                    rest
+                })
+                .into_inner()
+            })
+            .rev()
+            .reduce(|right, mut left| {
+                left.input.push(right);
+                left
+            })
+            .unwrap();
+
+        expr.input.push(succ);
+        Ok((rest, expr))
+    }
+}
+
+struct PostLeft<'a, G: PrecedenceGraph>(&'a G, G::P);
+impl<'a, I, G> Parser<I, Tree> for PostLeft<'a, G>
+where
+    I: ParserInput + Compare<&'a str>,
+    G: PrecedenceGraph,
+{
+    fn parse(&self, i: I) -> ParserResult<I, Tree, ParseError> {
+        let succ = Precs(self.0, self.0.succ(self.1));
+        let post = Or(
+            Inner(self.0, self.1, Fixity::Postfix),
+            (
+                Inner(self.0, self.1, Fixity::Infix(Associativity::Left)),
+                &succ,
+            ),
+        );
+        let (rest, succ) = succ.parse(i)?;
+        let (rest, inners) = Many1(post).parse(rest)?;
+        let mut expr = inners
+            .into_iter()
+            .map(|e| {
+                e.map_right(|(mut rest, last)| {
+                    rest.input.push(last);
+                    rest
+                })
+                .into_inner()
+            })
+            .rev()
+            .reduce(|left, mut right| {
+                right.input.insert(0, left);
+                right
+            })
+            .unwrap();
+
+        expr.input.insert(0, succ);
+        Ok((rest, expr))
+    }
+}
+
+impl<'a> Compare<&'a str> for &'a str {
+    fn compare(&self, other: &'a str) -> bool {
+        *self == other
+    }
+}
+
+pub fn parse<'a, G: PrecedenceGraph>(graph: &'a G, input: &'a str) -> Result<Tree, ParseError> {
+    let (unparsed, expr) = Expr(graph).parse(input)?;
+    if unparsed.is_empty() {
+        Ok(expr)
+    } else {
+        Err(ParseError::UnparsedInput)
     }
 }
 
@@ -189,10 +640,12 @@ mod tests {
     use petgraph::dot::Dot;
 
     use super::*;
-    #[test]
-    fn test() {
-        let mut graph = PrecedenceGraph::new();
+
+    fn make_graph() -> MappedPrecedenceGraph {
+        let mut graph = MappedPrecedenceGraph::new();
         let parenthesis = Operator::new(Fixity::Closed, "(_)");
+        let b = Operator::new(Fixity::Closed, "b");
+        let n = Operator::new(Fixity::Closed, "n");
         let add = Operator::new(Fixity::Infix(Associativity::Left), "_+_");
         let sub = Operator::new(Fixity::Infix(Associativity::Left), "_-_");
         let eq = Operator::new(Fixity::Infix(Associativity::Left), "_==_");
@@ -209,12 +662,59 @@ mod tests {
         graph.add(&eq, PrecedenceRelation::Tighter, &land);
         graph.add(&land, PrecedenceRelation::Looser, &parenthesis);
         graph.add(&eq, PrecedenceRelation::Looser, &parenthesis);
-
+        graph.add(&b, PrecedenceRelation::Equal, &parenthesis);
+        graph.add(&n, PrecedenceRelation::Equal, &parenthesis);
+        graph
+    }
+    #[test]
+    fn test_graph_shape() {
+        let graph = make_graph();
         assert!(graph.is_acyclic());
         println!(
             "{:?}",
             Dot::with_config(&graph.graph, &[Config::EdgeNoLabel])
         );
         println!("{:?}", graph.map);
+    }
+
+    #[test]
+    fn test_graph_trait() {
+        let graph = make_graph();
+        let (graph, map) = (graph.graph, graph.map);
+        assert_eq!(graph.node_count(), 6);
+        println!("{:?}", map);
+        for prec in graph.node_indices() {
+            println!("{:?} {:?}", prec, graph.succ(prec));
+        }
+    }
+
+    #[test]
+    fn test_combinator() {
+        let i = "ab  b b a ab ab";
+
+        println!("{:?}", (Tag("ab"), SkipSpace(), Tag("b")).parse(i).unwrap());
+        println!("{:?}", Alt(vec![Tag("ab"), Tag("b")]).parse(i).unwrap());
+        println!(
+            "{:?}",
+            Many1(Alt(vec![
+                (SkipSpace(), Tag("ab")),
+                (SkipSpace(), Tag("b")),
+                (SkipSpace(), Tag("a"))
+            ]))
+            .parse(i)
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn test_parser() {
+        let graph = make_graph();
+        let i = "if b && n + n == n ! then n else ( n + n - n )";
+        let tree = parse(&graph.graph, i);
+        if let Err(ParseError::UnexpectedToken(n)) = tree {
+            let n = i.chars().count() - n;
+            println!("{:#?}", &i[n..]);
+        }
+        println!("{:#?}", tree);
     }
 }
